bc91f97907ca65a6175da4709568f1f141631fab
Beumer <r.m.beumer@student.tue.nl>
2017-01-14 15:49:48 +0100

    test other db writing option

--- finalassignmentapp/views.py
+++ finalassignmentapp/views.py
@@ -27,8 +27,34 @@ def userView(request):
             usernaam = request.POST.get('username', '')
             wachtwoord = request.POST.get('password', '')
             ingelogd = True
-            user_obj = Users(us_name=usernaam, us_password=wachtwoord, us_loggedin=ingelogd)
-            user_obj.save()
+            #user_obj = Users(us_name=usernaam, us_password=wachtwoord, us_loggedin=ingelogd)
+            #user_obj.save()
+
+
+            # open db
+            try:
+                conn = psycopg2.connect(dbname='d3enlk294pbi15', user='tleawaslframmo',
+                                        host='ec2-107-22-236-252.compute-1.amazonaws.com',
+                                        password='6f347648c1a4b2a5aeb99ef699a0aba2d4f4a64d614b68c6159e180dc2b60e3e')
+                print('Opened DB successfully')
+            except:
+                print(datetime.now(), "Unable to connect to the database")
+                logging.exception("Unable to open the database")
+                return
+            else:
+                cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
+            # write data to database
+            cur.execute("""INSERT INTO finalassignmentapp_users (us_name, us_password, us_loggedin,)
+                            VALUES (%s, %s, %s)""",
+                        (usernaam, wachtwoord, ingelogd))
+
+            conn.commit()
+            cur.close()
+            conn.close()
+
+            print("Data Written", datetime.now())
+
 
             return HttpResponseRedirect(reverse('finalassignment:index.html')) # Redirect after POST
     else:
