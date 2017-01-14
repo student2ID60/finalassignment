b6af27076a0f087ea1657487959bc4dad14310a1
Beumer <r.m.beumer@student.tue.nl>
2017-01-14 14:43:02 +0100

    updated url patterns

--- finalassignment/urls.py
+++ finalassignment/urls.py
@@ -21,6 +21,6 @@ urlpatterns = [
     # url(r'^admin/', admin.site.urls), this could be used for the django admin console to add and remove data to the database
     url(r'^$', finalassignmentapp.views.home, name='home'),
     url(r'^index.html$', finalassignmentapp.views.home, name='home'),
-    #url(r'^index.html$', finalassignmentapp.views.home, name='userView'),
+    url(r'^supermarkets.html$', finalassignmentapp.views.home, name='userView'),
     url(r'^favourites.html$', finalassignmentapp.views.favourites, name='favourites'),
 ]
