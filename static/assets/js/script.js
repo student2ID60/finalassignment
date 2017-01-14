037d4bb2edc477296e56e6095784e47e94969f09
Beumer <r.m.beumer@student.tue.nl>
2017-01-14 23:30:37 +0100

    update

--- static/assets/js/script.js
+++ static/assets/js/script.js
@@ -1,25 +1,29 @@
 $(document).ready(function() {
 
-  // show selected tab content
-  $("#tab-home").click(function() {
+    // first only show home content
     $(".tab-content").hide();
     $(".tab-content-home").show();
-  });
-  $("#tab-lists").click(function() {
+
+    // show selected tab content
+    $("#tab-home").click(function() {
+    $(".tab-content").hide();
+    $(".tab-content-home").show();
+    });
+    $("#tab-lists").click(function() {
     $(".tab-content").hide();
     $(".tab-content-lists").show();
-  });
-  $("#tab-favourites").click(function() {
+    });
+    $("#tab-favourites").click(function() {
     $(".tab-content").hide();
     $(".tab-content-favourites").show();
-  });
-  $("#tab-recipes").click(function() {
+    });
+    $("#tab-recipes").click(function() {
     $(".tab-content").hide();
     $(".tab-content-recipes").show();
-  });
-  $("#tab-supermarkets").click(function() {
+    });
+    $("#tab-supermarkets").click(function() {
     $(".tab-content").hide();
     $(".tab-content-supermarkets").show();
-  });
+    });
 
 });
