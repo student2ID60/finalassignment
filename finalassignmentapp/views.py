d747ba40bfcb23e87ce154f083b38c1175991257
Beumer <r.m.beumer@student.tue.nl>
2017-01-15 20:29:13 +0100

    test

--- finalassignmentapp/views.py
+++ finalassignmentapp/views.py
@@ -22,60 +22,68 @@ def home(request):
 def userView(request):
     if request.method == 'POST':  # If the forms has been sumbitted
 
-        message = ""
-        form = UserForm(request.POST)
+        command = request.POST.get('command', '')
 
-        username = request.POST.get('username', '')
-        password = request.POST.get('password', '')
-        loggedin = True
-        #user_obj = Users(us_name=username, us_password=password, us_loggedin=loggedin)
-        #user_obj.save()
+        if command == "login":
+            message = ""
+            form = UserForm(request.POST)
 
-        # open database
-        try:
-            conn = psycopg2.connect(dbname='d3enlk294pbi15', user='tleawaslframmo',
-                                    host='ec2-107-22-236-252.compute-1.amazonaws.com',
-                                    password='6f347648c1a4b2a5aeb99ef699a0aba2d4f4a64d614b68c6159e180dc2b60e3e')
-            print('Opened DB successfully')
-        except:
-            print(datetime.now(), "Unable to connect to the database")
-            logging.exception("Unable to open the database")
-            return
-        else:
-            cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+            username = request.POST.get('username', '')
+            password = request.POST.get('password', '')
+            loggedin = True
+            #user_obj = Users(us_name=username, us_password=password, us_loggedin=loggedin)
+            #user_obj.save()
 
-        stmt = "SELECT us_password FROM finalassignmentapp_users WHERE us_name=%s"
-        params = (username,)
-        cur.execute(stmt,params)
-        username_exists = cur.fetchone()
-
-        current_user = {'id': 0, 'us_name': '', 'us_password': '', 'us_loggedin': False}
-
-        if username_exists is None:
-            # write data to database
-            cur.execute("""INSERT INTO finalassignmentapp_users (us_name, us_password, us_loggedin)
-                            VALUES (%s, %s, %s)""",
-                        (username, password, loggedin))
-            conn.commit()
-            print("Data Written", datetime.now())
-            current_user = Users.objects.last()  # niet zo netjes
-
-        else :
-            required_password = username_exists[0];
-            if password == required_password:
-                stmt = "UPDATE finalassignmentapp_users SET us_loggedin=TRUE WHERE us_name=%s"
-                params = (username,)
-                cur.execute(stmt,params)
-                current_user = Users.objects.filter(us_name=username).first();
+            # open database
+            try:
+                conn = psycopg2.connect(dbname='d3enlk294pbi15', user='tleawaslframmo',
+                                        host='ec2-107-22-236-252.compute-1.amazonaws.com',
+                                        password='6f347648c1a4b2a5aeb99ef699a0aba2d4f4a64d614b68c6159e180dc2b60e3e')
+                print('Opened DB successfully')
+            except:
+                print(datetime.now(), "Unable to connect to the database")
+                logging.exception("Unable to open the database")
+                return
             else:
-                message = "Wrong password"
+                cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
+            stmt = "SELECT us_password FROM finalassignmentapp_users WHERE us_name=%s"
+            params = (username,)
+            cur.execute(stmt,params)
+            username_exists = cur.fetchone()
+
+            current_user = {'id': 0, 'us_name': '', 'us_password': '', 'us_loggedin': False}
+
+            if username_exists is None:
+                # write data to database
+                cur.execute("""INSERT INTO finalassignmentapp_users (us_name, us_password, us_loggedin)
+                                VALUES (%s, %s, %s)""",
+                            (username, password, loggedin))
+                conn.commit()
+                print("Data Written", datetime.now())
+                current_user = Users.objects.last()  # niet zo netjes
+
+            else :
+                required_password = username_exists[0];
+                if password == required_password:
+                    stmt = "UPDATE finalassignmentapp_users SET us_loggedin='yes' WHERE us_name=%s"
+                    params = (username,)
+                    cur.execute(stmt,params)
+                    current_user = Users.objects.filter(us_name=username).first();
+                else:
+                    message = "Wrong password"
 
 
-        cur.close()
-        conn.close()
+            cur.close()
+            conn.close()
 
-        return TemplateResponse(request, 'index.html', {'data': current_user, 'login_message': message})
-        #return HttpResponseRedirect('../lists.html')  # Redirect after POST
+            return TemplateResponse(request, 'index.html', {'data': current_user, 'login_message': message})
+            #return HttpResponseRedirect('../lists.html')  # Redirect after POST
+
+        elif command == "make_list":
+            message = "make list gelukt"
+            return TemplateResponse(request, 'index.html', {'login_message': message})
+
 
     else:
         form = UserForm()  # An unbound form
