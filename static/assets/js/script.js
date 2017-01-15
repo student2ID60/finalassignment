8fbf9b99ac523f4e67bc15c5d663c31a28b2deba
Beumer <r.m.beumer@student.tue.nl>
2017-01-15 12:09:06 +0100

    test10

--- static/assets/js/script.js
+++ static/assets/js/script.js
@@ -1,40 +1,5 @@
 $(document).ready(function() {
 
-    // first only show home content
-    $(".tab-content").hide();
-    $(".tab-content-home").show();
 
-    // show selected tab content
-    $("#tab-home").click(function() {
-        $(".tab-item").removeClass("active");
-        /* when there is no home tab (title used for this), comment out this line:
-        $("#tab-home").addClass("active");*/
-        $(".tab-content").hide();
-        $(".tab-content-home").show();
-    });
-    $("#tab-lists").click(function() {
-        $(".tab-item").removeClass("active");
-        $("#tab-lists").addClass("active");
-        $(".tab-content").hide();
-        $(".tab-content-lists").show();
-    });
-    $("#tab-favourites").click(function() {
-        $(".tab-item").removeClass("active");
-        $("#tab-favourites").addClass("active");
-        $(".tab-content").hide();
-        $(".tab-content-favourites").show();
-    });
-    $("#tab-recipes").click(function() {
-        $(".tab-item").removeClass("active");
-        $("#tab-recipes").addClass("active");
-        $(".tab-content").hide();
-        $(".tab-content-recipes").show();
-    });
-    $("#tab-supermarkets").click(function() {
-        $(".tab-item").removeClass("active");
-        $("#tab-supermarkets").addClass("active");
-        $(".tab-content").hide();
-        $(".tab-content-supermarkets").show();
-    });
 
 });
